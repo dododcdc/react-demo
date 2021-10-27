77b78a95c14b6c7a0d0c2d92040b60e1519c1c68
dododcdc <dcdc3166@foxmail.com>
2021-10-28 02:04:19 +0800

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,8 +17,7 @@ var res = true
 const rr = () => {
     if (!res) {
         return <div>ggg {name}</div>
-    }
-    else {
+    } else {
         return <div>mmm {name}</div>
     }
 
@@ -30,38 +29,39 @@ var tag = <h1>{rr()}</h1>
 
 // 遍历数据
 const colors = [
-    {id:1 ,color:'red'},
-    {id:2 ,color:'blue'},
-    {id:3 ,color:'yellow'}
+    {id: 1, color: 'red'},
+    {id: 2, color: 'blue'},
+    {id: 3, color: 'yellow'}
 ]
 
 tag = (
     <ul>
         {/*一定要把key加上 不加也会显示，但控制台会报错*/}
-        {colors.map(item => <li key={item.id}>{item.color}</li> )}
+        {colors.map(item => <li key={item.id}>{item.color}</li>)}
     </ul>
 )
 
 // 行内样式 不推荐这样 注意style里面是
-tag = (<h1 style={{background:"blue"}}>ggg</h1>)
+tag = (<h1 style={{background: "blue"}}>ggg</h1>)
 
 // 使用类名添加样式 注意 以前的class在这里叫className
 tag = (<h1 className="tag">how are you</h1>)
 
 // 渲染到index.html id为root的容器中
-ReactDOM.render(tag,document.getElementById("root"))
+ReactDOM.render(tag, document.getElementById("root"))
 
 // 下面基本都可以推荐，类创建组件比较不错
 // 使用函数创建组件 函数名称必须大写字母开头 ， 函数必须有返回值，可以返回null(表示该住建的结构)
 function Foo() {
     return (<h2> 你好 </h2>)
 }
+
 // 使用函数创建 组件 简写
 const Simple = () => (<div>简写真方便</div>)
 
 
 // 渲染函数组件,也可以用双标签<Foo></Foo>
-ReactDOM.render(<Simple />,document.getElementById("root"))
+ReactDOM.render(<Simple/>, document.getElementById("root"))
 
 
 // 使用类创建组件 类名必须以大写字母开头 ,要继承React.Component 最少 有一个 render() 且必须有返回值
@@ -73,23 +73,25 @@ class Bar extends React.Component {
 
 }
 
-ReactDOM.render(<Bar />,document.getElementById('root'))
+ReactDOM.render(<Bar/>, document.getElementById('root'))
 
 
 // 在顶部引入外部组件good
-ReactDOM.render(<Good />,document.getElementById('root'))
+ReactDOM.render(<Good/>, document.getElementById('root'))
 
 
 // 事件绑定
 function doit() {
     alert("hello")
 }
+
 // 1. 创建 一个类组件
 class Hand extends React.Component {
     handClick() {
         alert("gogogo")
     }
-    render () {
+
+    render() {
         // this.调的函数是属于Hand的 doit是Hand外面的函数
         // 如果组件是函数组件那么是没有this的，直接调用
         // 其他事件 onMouseEnter onFocus 等等 都要用驼峰命名
@@ -99,22 +101,24 @@ class Hand extends React.Component {
         </div>)
     }
 }
+
 //渲染
-ReactDOM.render(<Hand />,document.getElementById("root"))
+ReactDOM.render(<Hand/>, document.getElementById("root"))
 
 
 // 事件处理之阻止浏览器的默认行为 比如 点击a标签做一件事 但是 不跳转页面(浏览器的默认行为)
 
 class Stop extends React.Component {
-    handClick () {
+    handClick() {
         alert("阻止浏览器默认行为")
     }
+
     render() {
         return (<button onClick={this.handClick}>阻止行为</button>)
     }
 }
 
-ReactDOM.render(<Stop />,document.getElementById('root'))
+ReactDOM.render(<Stop/>, document.getElementById('root'))
 
 // 有状态组件和无状态组件 函数组件就是无状态组件 类组件就是有状态组件 状态即stage
 
@@ -136,19 +140,142 @@ class Student extends React.Component {
         name: 'tom',
         age: 6
     }
+
     // 状态是私有的，只能在组件内部获取
-    render () {
+    render() {
         return (<div>hello {this.state.name} 年龄：{this.state.age}
             <button onClick={() => {
                 // 修改 必须在setState里面进行 ，这个函数可以抽取到render外面去
                 this.setState({
                     age: this.state.age + 1
                 })
-            }}>+</button></div>)
+            }}>+
+            </button>
+        </div>)
     }
 }
 
-ReactDOM.render(<Student />,document.getElementById('root'))
+ReactDOM.render(<Student/>, document.getElementById('root'))
+
+// 将上面onclick中的方法抽取出来 注意this那个玩意儿！！！！！！！！！！！！！！！！！！！！！！！！！！
+
+class Stu extends React.Component {
+    state = {
+        name: 'jack',
+        age: 55
+    }
+
+    addAge() {
+        this.setState({
+            age: this.state.age + 1
+        })
+    }
+
+    render() {
+        return (<div>hello {this.state.name} 年龄：{this.state.age}
+            {/*注意 onclick里面一定不要写 this.addAge
+            如果非要那么写,有两个解决方案
+                1.可以先在constructor里面把this和这个函数绑定一下 this.addAge = this.addAge.bind(this)
+                或者2.改造 addAge函数的写法 addAge = () => { ..... }
+            */}
+            <button onClick={() => this.addAge()}>+</button>
+        </div>)
+    }
+}
+
+ReactDOM.render(<Stu/>, document.getElementById('root'))
+
+// 受控组件 ， 所有变化的状态统一绑定到state,有点蠢啊，vue直接自动绑定
+
+class InputContro extends React.Component{
+
+    state = {
+        txt:'',
+        t:''
+        // state 里面有多个 下面的绑定方法就要写很多个 ，有点蠢 ， 下面 介绍如何一个方法解决
+    }
+
+    setTxt = (e) => {
+        this.setState({
+            txt: e.target.value
+        })
+        console.log(e.target.value ,"state" ,this.state.txt)
+
+    }
+    setT = (e) => {
+        this.setState({
+            t: 100
+        })
+    }
+
+    render() {
+        return (<div>
+            <input type='text' value={this.state.txt} onChange={this.setTxt} />
+        </div>)
+    }
+}
+
+ReactDOM.render(<InputContro />,document.getElementById('root'))
+
+// 受控组件 一个方法解决所有绑定 包括 像 checkbox那种值不是value而是checked
+// 首先 要给每个控件 添加一个name属性 name属性要和state里面的一致
+
+class Ct extends React.Component {
+
+    state = {
+        txt:'',
+        checked:false
+    }
+    updateValue = (e) => {
+        const name = e.target.name
+        const type = e.target.type
+        const val = type === 'checkbox' ? e.target.checked : e.target.value
+        this.setState({
+            [name] : val
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+               {/*注意这里的name的值要和state里面保持一样 这样[name] 才能匹配上*/}
+                <input name='txt' type='text' value={this.state.txt} onChange={this.updateValue} />
+                <input name='checked' type='checkbox' checked={this.state.checked} onChange={this.updateValue}/>
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(<Ct />,document.getElementById('root'))
+
+
+// 非受控组件 不绑定到 state上
+
+class NotBind extends React.Component{
+    constructor () {
+        super()
+        //创建ref
+        this.txtRef = React.createRef()
+    }
+
+    getVal = () => {
+        console.log(this.txtRef.current.value)
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref={this.txtRef} />
+                <button onClick={this.getVal}>获取输入框的值 consloe.log</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<NotBind />,document.getElementById('root'))
+
 
 
 // import './index.css';
